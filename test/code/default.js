2ec33b6cfb29a13d268bfc32be0ecb77346d5304
ugate <ugate.public@gmail.com>
2019-01-16 09:59:39 -0500

    [REFACTOR] : Default tests added for missing pathBase for compile-time and runtime errors

--- test/code/default.js
+++ test/code/default.js
@@ -21,24 +21,42 @@ class Tester {
     return Main.baseTest(opts.compile, engine, partials, true, opts.render);
   }
 
-  static async htmlPartialsFetchHttpServerCompiletime() {
+  static async htmlPartialsFetchHttpsServerCompiletimeRead() {
     const basePath = 'test/views/partials', svr = await Main.httpsServer(basePath);
     const opts = baseOptions();
-    opts.render.pathBase = svr.url; // partials will be served from this URL
+    opts.compile.pathBase = svr.url; // partials will be served from this URL during compile-time
     const engine = new Engine(opts.compile, JsFrmt, LOGGER);
     // partials should be fetched via the HTTPS server during compilation via the cache read/fetch
     const partials = await Main.getFiles(basePath, false); // false will only return the partial names w/o content
-    await Main.baseTest(opts.compile, engine, partials, true, opts.render); // true to init read/fetch
+    await Main.baseTest(opts.compile, engine, partials, true, opts.render); // true to registerPartials at compile-time
     await svr.close();
   }
 
-  static async htmlPartialsFetchHttpServerRuntime() {
+  static async htmlPartialsFetchHttpsServerCompiletimeReadNoPathError() {
     const basePath = 'test/views/partials', svr = await Main.httpsServer(basePath);
     const opts = baseOptions();
-    opts.render.pathBase = svr.url; // partials will be served from this URL
     const engine = new Engine(opts.compile, JsFrmt, LOGGER);
-    // partials should be fetched via the HTTPS server as includes are encountered during rendering
-    await Main.baseTest(opts.compile, engine, null, false, opts.render);
+    const partials = await Main.getFiles(basePath, false); // false will only return the partial names w/o content
+    await Main.baseTest(opts.compile, engine, partials, true, opts.render); // true to registerPartials at compile-time
+    await svr.close();
+  }
+
+  static async htmlPartialsFetchHttpsServerRuntimeRead() {
+    const basePath = 'test/views/partials', svr = await Main.httpsServer(basePath);
+    const opts = baseOptions();
+    opts.render.pathBase = svr.url; // partials will be served from this URL during runtime
+    const engine = new Engine(opts.compile, JsFrmt, LOGGER);
+    // partials should be fetched via the HTTPS server when includes are encountered during rendering
+    await Main.baseTest(opts.compile, engine, null, false, opts.render); // false to prevent compile-time registerPartials
+    await svr.close();
+  }
+
+  static async htmlPartialsFetchHttpsServerRuntimeReadNoPathError() {
+    const basePath = 'test/views/partials', svr = await Main.httpsServer(basePath);
+    const opts = baseOptions();
+    const engine = new Engine(opts.compile, JsFrmt, LOGGER);
+    // partials should be fetched via the HTTPS server when includes are encountered during rendering
+    await Main.baseTest(opts.compile, engine, null, false, opts.render); // false to prevent compile-time registerPartials
     await svr.close();
   }
 }
@@ -53,9 +71,23 @@ if (!Main.usingTestRunner()) {
 
 function baseOptions() {
   return {
-    compile: {},
+    compile: {
+      rejectUnauthorized: false
+    },
     render: {
       rejectUnauthorized: false
     }
   };
+}
+
+async function httpsServerTest(compileOrRuntime, partialFiles) {
+  const basePath = 'test/views/partials', svr = await Main.httpsServer(basePath);
+  const opts = baseOptions();
+  if (compileOrRuntime === 'compiletime') opts.compile.pathBase = svr.url; // partials will be served from this URL
+  else if (compileOrRuntime === 'runtime') opts.render.pathBase = svr.url; // partials will be served from this URL
+  const engine = new Engine(opts.compile, JsFrmt, LOGGER);
+  // partials should be fetched via the HTTPS server during compilation via the cache read/fetch
+  const partials = partialFiles ? await Main.getFiles(basePath, partialFiles === 'prefetch') : null; // false will only return the partial names w/o content
+  await Main.baseTest(opts.compile, engine, partials, true, opts.render); // true to init read/fetch
+  await svr.close();
 }
--- test/default.js
+++ test/default.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { Lab, PLAN, TEST_TKO } = require('./code/_main.js');
+const { expect, Lab, PLAN, TEST_TKO, LOGGER } = require('./code/_main.js');
 const Tester = require('./code/default');
 const lab = Lab.script();
 exports.lab = lab;
 // ESM uncomment the following lines...
-// TODO : import { Lab, PLAN, TEST_TKO } from './code/_main.mjs';
+// TODO : import { expect, Lab, PLAN, TEST_TKO, LOGGER } from './code/_main.mjs';
 // TODO : import * as Tester from './code/default.mjs';
 // TODO : export * as lab from lab;
 
@@ -16,6 +16,27 @@ const plan = `${PLAN} Default`;
 lab.experiment(plan, () => {
 
   lab.test(`${plan}: HTML - registerPartials`, { timeout: TEST_TKO }, Tester.htmlRegisterPartials);
-  lab.test(`${plan}: HTML - Partials Fetch From HTTPS Server (compile-time)`, { timeout: TEST_TKO }, Tester.htmlPartialsFetchHttpServerCompiletime);
-  lab.test(`${plan}: HTML - Partials Fetch From HTTPS Server (runtime)`, { timeout: TEST_TKO }, Tester.htmlPartialsFetchHttpServerRuntime);
+  lab.test(`${plan}: HTML - Partials Fetch From HTTPS Server (compile-time)`, { timeout: TEST_TKO }, Tester.htmlPartialsFetchHttpsServerCompiletimeRead);
+  lab.test(`${plan}: HTML - Partials Fetch From HTTPS Server (compile-time ERROR missing "options.pathBase")`, { timeout: TEST_TKO }, flags => {
+    return new Promise(resolve => {
+      flags.onUnhandledRejection = err => {
+        expect(err).to.be.error();
+        if (LOGGER.info) LOGGER.info(`Expected error message received for: ${err.message}`);
+        resolve();
+      };
+      return Tester.htmlPartialsFetchHttpsServerCompiletimeReadNoPathError();
+    });
+  });
+  lab.test(`${plan}: HTML - Partials Fetch From HTTPS Server (runtime)`, { timeout: TEST_TKO }, Tester.htmlPartialsFetchHttpsServerRuntimeRead);
+  lab.test(`${plan}: HTML - Partials Fetch From HTTPS Server (runtime ERROR missing "options.pathBase")`, { timeout: TEST_TKO }, flags => {
+    return new Promise(resolve => {
+      flags.onUnhandledRejection = err => {
+        expect(err).to.be.error();
+        expect(err.code).to.equal('ERR_INVALID_URL');
+        if (LOGGER.info) LOGGER.info(`Expected error message received for: ${err.message}`);
+        resolve();
+      };
+      return Tester.htmlPartialsFetchHttpsServerRuntimeReadNoPathError();
+    });
+  });
 });
